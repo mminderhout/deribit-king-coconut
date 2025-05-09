44be97c32fbe07492127ada2769b8262f3585f12
m.minderhout <m.minderhout@vbadvisory.nl>
2025-05-09 10:43:41 +0200

    find first date all products were trading on deribit

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
+import requests
+from datetime import datetime
+
+COST = {
+    'ETH': 0.0013371,
+    'BTC': 0.00005181,
+    'PAX': 0.0015856,
+    'SOL': 0.020196,
+    'XRP': 7.2942,
+    'ADA': 7.3376
+}
+INSTRUMENTS = [
+    'BTC-PERPETUAL',
+    'ETH-PERPETUAL',
+    'PAXG_USDC-PERPETUAL',
+    'SOL_USDC-PERPETUAL',
+    'XRP_USDC-PERPETUAL',
+    'ADA_USDC-PERPETUAL'
+]
 
 
+def find_start_date():
+    url = "https://www.deribit.com/api/v2/public/get_instrument"
+    creation_timestamps = []
+    for instrument in INSTRUMENTS:
+        params = {"instrument_name": instrument}
+        response = requests.get(url, params=params)
+        creation_timestamps.append(response.json()['result']['creation_timestamp'])
+    print(f'The earliest all products were available is {datetime.fromtimestamp(max(creation_timestamps) / 1000).strftime('%Y-%m-%d %H:%M:%S')}.')
+    return max(creation_timestamps)
 
 def main():
-    pass
+    date_start = find_start_date()
+
 
 if __name__ == "__main__":
     main()
